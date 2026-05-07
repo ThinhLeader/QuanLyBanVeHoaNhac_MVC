935ffd84ead3cf7d2a87ffc017da7147b60f772b
LeThinh <levanthinh32.thptllqt@gmail.com>
2026-05-07 22:55:27 +0700

    khoi tao du an

---
+++ QuanLyVeHoaNhac/QuanLyVeHoaNhac.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyVeHoaNhac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
